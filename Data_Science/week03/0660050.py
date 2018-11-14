ccf0573e7e9a191b1f57c48ace27aacaf2a74c30
Tony Hsiao <tonycar12002@gmail.com>
2018-11-14 15:54:35 +0800

    update

--- Data_Science/week03/0660050.py
+++ Data_Science/week03/0660050.py
@@ -3,6 +3,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.svm import SVC
+from sklearn import datasets
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
 # Importing the dataset
 dataset_train = pd.read_csv('train.csv', header=None)
@@ -12,7 +18,7 @@ dataset_test = pd.read_csv('test.csv', header=None)
 # Take all columns except last one
 X_train = dataset_train.iloc[:, :-1]
 X_test = dataset_test.iloc[:, :]
-y_train = dataset_train.iloc[:, dataset_train.shape[1]-1]
+y = dataset_train.iloc[:, dataset_train.shape[1]-1]
 
 
 # Encoding categorial data
@@ -36,6 +42,39 @@ drop.sort(reverse=True)
 for row in drop:
     dataset = dataset.drop(row, axis = 1)
 
+X = dataset[:train_objs_num]
 
-X_train = dataset[:train_objs_num]
-X_test = dataset[train_objs_num:]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+#feature normalize
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+svm = SVC(kernel='rbf')
+svm.fit(X_train_std, y_train)
+y_pred = svm.predict(X_test_std)
+
+
+print ("Misclassified sample %d" % (y_test!=y_pred).sum())
+print ("Accuracy: ", accuracy_score(y_test, y_pred))
+precision, recall, fscore, support = precision_recall_fscore_support(y_test, y_pred, average='micro')
+print ("precision: ", precision)
+print ("recall: ", recall)
+print ("fscore: ", fscore)
+
+
+
+test_data = dataset[train_objs_num:]
+test_std = sc.transform(test_data)
+test_std_pred = svm.predict(test_std)
+
+import csv
+with open('/output.csv', 'w+') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['ID', 'and'])
+    for i in range(len(test_std_pred)):
+        writer.writerow([i, test_std_pred[i]])
+
+
